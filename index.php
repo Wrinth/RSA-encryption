f8c7b7881c154e7c7360d10638fd4968821ec949
Wrinth <johnl4112@gmail.com>
2018-08-09 14:37:54 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+?>
+<html lang="en-US">
+<head>
+  	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- mobile friend option -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Encrypt</title>
+
+	<!--bootstrap core-->
+	<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
+
+	<!--additional css-->
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+
+	<!-- Js libraries-->
+	<script type="text/javascript" src="./script/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./script/jquery.min.js"></script>
+</head>
+
+<body>
+	<!--start-header-->
+	<div class="header">
+		<div class="container">
+			<div class="head">
+			<div class="navigation">
+				 <span class="menu"></span> 
+					<ul class="navig">
+						<li><a href="#">Log In</a></li>
+						<li><a href="#">Sign Up</a></li>
+					</ul>
+			</div>
+			
+			</div>
+		</div>	
+
+	
+	<!--slide-starts-->
+	<div class="slide">
+		<div class="container">
+
+		</div>
+	</div>	
+	<!--slide-end-->
+	<!--footer-starts-->
+	<div class="footer">
+		<div class="container">
+			<div class="footer-text">
+				<p>© 2016 Encrypt. All Rights Reserved | Design by  <a href="https://www.linkedin.com/in/ho-yeung-lai-81b54750" target="_blank">John Lai</a> </p>
+			</div>
+		</div>
+	</div>
+	<!--footer-end-->
+</body>
+</html>
